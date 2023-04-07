d09c81a51d183af17ad7e6f07a3bd1bc8264874d
xRalph18 <rafal.machnik4@gmail.com>
2023-04-07 13:01:57 +0200

    json reading

--- RunTimeTracker/Program.cs
+++ RunTimeTracker/Program.cs
@@ -1,7 +1,28 @@
-﻿class Program
+﻿using Newtonsoft.Json;
+using RunTimeTracker;
+using System.Diagnostics;
+using System.Text.Json.Serialization;
+
+class Program
 {
     public static void Main()
     {
+        Console.Write("Podaj nazwę aplikacji, którą chcesz ślidzić: ");
+        string appName = Console.ReadLine();
+        string dataPath = "TimeData.json";
+        List<TimeSaveModel> timeData;
+        string dataString;
 
+        if (File.Exists(dataPath))
+        {
+            dataString = File.ReadAllText(dataPath);
+            timeData = JsonConvert.DeserializeObject<List<TimeSaveModel>>(dataString);
+        }
+        else
+        {
+            File.WriteAllText(dataPath, "");
+        }
+
+        var trackedApp = Process.GetProcessesByName(appName).FirstOrDefault();
     }
 }
---
+++ RunTimeTracker/TimeSaveModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RunTimeTracker
+{
+    class TimeSaveModel
+    {
+        public string AppName { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime ExitTime { get; set; }
+
+        public TimeSaveModel(DateTime startTime, DateTime exitTime)
+        {
+            StartTime = startTime;
+            ExitTime = exitTime;
+        }
+    }
+}
