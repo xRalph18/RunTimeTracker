005176d244175e42255cb5313310268c2b6fbb6a
xRalph18 <rafal.machnik4@gmail.com>
2023-04-07 15:38:48 +0200

    Woring App v0.1

--- RunTimeTracker/Program.cs
+++ RunTimeTracker/Program.cs
@@ -1,43 +1,76 @@
 ﻿using Newtonsoft.Json;
 using RunTimeTracker;
 using System.Diagnostics;
-using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Text.Json.Serialization;
 
 class Program
 {
     public static void Main()
     {
-        Console.Write("Podaj nazwę aplikacji, którą chcesz ślidzić: ");
-        string appName = Console.ReadLine();
+        Console.Write("(track) / (check): ");
+        var task = Console.ReadLine();
 
-        var trackedApp = Process.GetProcessesByName(appName).FirstOrDefault();
         string dataPath = "TimeData.json";
         string dataString;
-        DateTime startTime = trackedApp.StartTime;
 
         var settings = new JsonSerializerSettings
         {
             NullValueHandling = NullValueHandling.Ignore,
         };
 
-        if (File.Exists(dataPath))
+        if (task == "track")
         {
-            dataString = File.ReadAllText(dataPath);
-            var timeData = JsonConvert.DeserializeObject<List<TimeSaveModel>>(dataString, settings) ?? new List<TimeSaveModel>();
+            Console.Write("Podaj nazwę aplikacji, którą chcesz ślidzić: ");
+            string appName = Console.ReadLine();
 
-            trackedApp.WaitForExit();
+            var trackedApp = Process.GetProcessesByName(appName).FirstOrDefault();
+            DateTime startTime = trackedApp.StartTime;
 
-            DateTime exitTime = DateTime.Now;
-            timeData.Add(new TimeSaveModel(appName, startTime, exitTime));
+            if (File.Exists(dataPath))
+            {
+                dataString = File.ReadAllText(dataPath);
+                var timeData = JsonConvert.DeserializeObject<List<TimeSaveModel>>(dataString, settings) ?? new List<TimeSaveModel>();
 
-            dataString = JsonConvert.SerializeObject(timeData);
-            File.WriteAllText(dataPath, dataString);
+                trackedApp.WaitForExit();
+
+                DateTime exitTime = DateTime.Now;
+                timeData.Add(new TimeSaveModel(appName, startTime, exitTime));
+
+                dataString = JsonConvert.SerializeObject(timeData);
+                File.WriteAllText(dataPath, dataString);
+            }
+            else
+            {
+                File.WriteAllText(dataPath, "");
+                return;
+            }
+        }
+        else if (task == "check")
+        {
+            Console.Write("Podaj nazwę aplikacji, której czas chcesz sprawdzić: ");
+            var appToCheck = Console.ReadLine();
+
+            if (File.Exists(dataPath))
+            {
+                dataString = File.ReadAllText(dataPath);
+                var timeData = JsonConvert.DeserializeObject<List<TimeSaveModel>>(dataString, settings) ?? new List<TimeSaveModel>();
+                TimeSpan runTime = new TimeSpan();
+
+                foreach (var item in timeData.Where(n => n.AppName == appToCheck))
+                {
+                    runTime += item.ExitTime - item.StartTime;
+                }
+
+                Console.WriteLine(runTime);
+                Console.ReadKey();
+            }
+            else
+            {
+                File.WriteAllText(dataPath, "");
+                return;
+            }
         }
         else
         {
-            File.WriteAllText(dataPath, "");
             return;
         }
     }
